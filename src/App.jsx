4f96898f2878412261fc83cf3410b20231a309ca
thedistortedwajdan <wajdan.mohammad@gmail.com>
2025-07-06 18:51:03 +0500

    Setting up Outer boxes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import MainInfo from "./components/MainInfo.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="m-0 p-0 w-screen h-screen box-border bg-[#121212]">
+        <Sidebar />
+        <MainInfo />
+      </div>
+    </>
+  );
 }
 
 export default App;
+ 
---
+++ src/components/MainInfo.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function MainInfo() {
+  return <div className="text-white">MainInfo</div>;
+}
