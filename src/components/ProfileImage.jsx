6e451d58b58a044ea7255a482bb66e370a6bed55
thedistortedwajdan <wajdan.mohammad@gmail.com>
2025-07-12 14:49:24 +0500

    Sidebar basic skeleton is done

--- src/components/ProfileImage.jsx
+++ src/components/ProfileImage.jsx
@@ -2,7 +2,7 @@ import image from "../assets/me.jpeg";
 export default function ProfileImage() {
   return (
     <div>
-      <img src={image} alt="My Profile" className="w-32 h-32 rounded-[25%]" />
+      <img src={image} alt="My Profile" className="w-32 h-32" />
     </div>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,8 +2,19 @@ import ProfileImage from "./ProfileImage";
 
 export default function Sidebar() {
   return (
-    <div className="text-white bg-[#1E1E1F] rounded-[30px] border-[#383838] border-3 flex flex-col items-center">
-      <ProfileImage />
+    <div className="text-white bg-[#1E1E1F] border-[#383838] border-3  h-fit w-fit">
+      <div className="flex flex-col items-center m-5">
+        <ProfileImage />
+        <div>Muhammad Wajdan Ismail</div>
+        <div>Software Engineer</div>
+        <div className="bg-[#383838] w-[100%] h-[1px] "></div>
+        <ul className="items-start flex flex-col w-[100%] gap-5 ">
+          <li>email</li>
+          <li>github</li>
+          <li>linkedin</li>
+          <li>phone</li>
+        </ul>
+      </div>
     </div>
   );
 }
